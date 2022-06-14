7b16ec578022781edd288495f21c643c01f39c50
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2022-06-14 20:22:48 +0200

    Made Changes to the key system
    aka fixed a bug where if you had 1 key you could open all doors so i revamped the entire key system to a simpler more effective system

---
+++ MuseTeam7/Assets/Scripts/Key V2/Inventory.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+	public static bool[] keys = new bool[3];
+
+	private void Start()
+	{
+		keys[0] = true;
+	}
+
+}
